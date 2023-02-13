e5a74917b13fd5a2d0dcdd18b664556459a31be6
nick lucas <nick@nickthedev.com>
2023-02-13 00:31:17 -0800

    wip

--- functions/index.js
+++ functions/index.js
@@ -5,12 +5,58 @@ const functions = require("firebase-functions");
 const restfulApp = require("torqnlp-restful-app");
 
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+/**
+ * dataPreperationService is a task queue that is used to prepare data to create custom fine-tuned models. 
+ */
+exports.dataPreperationService = functions
+.runWith( {secrets: ["OPENAI_API_KEY"]})
+.tasks.taskQueue({
+  retryConfig: {
+    maxAttempts: 5,
+    minBackoffSeconds: 60,
+  },
+  rateLimits: {
+    maxConcurrentDispatches: 6,
+  },
+}).onDispatch(async (data) => {
+  // Here will be an imported function to excute the data preperation service.
+  return { success: true };
+});
+
+/**
+ * modelFineTuningService is a task queue that is used to fine-tune models.
+ */
+exports.modelFineTuningService = functions
+.runWith( {secrets: ["OPENAI_API_KEY"]})
+.tasks.taskQueue({
+  retryConfig: {
+    maxAttempts: 5,
+    minBackoffSeconds: 60,
+  },
+  rateLimits: {
+    maxConcurrentDispatches: 6,
+  },
+}).onDispatch(async (data) => {
+  // Here we will take pre pared data and create a fine-tuned model.
+  return { success: true };
+});
+
+/**
+ * modelEmbeddingService is a task queue that is used to create embeddings for models.
+ */
+exports.modelEmbeddingService = functions
+.runWith( {secrets: ["OPENAI_API_KEY"]})
+.tasks.taskQueue({
+  retryConfig: {
+    maxAttempts: 5,
+    minBackoffSeconds: 60,
+  },
+  rateLimits: {
+    maxConcurrentDispatches: 6,
+  },
+}).onDispatch(async (data) => {
+  // Here we will take pre pared data and create a fine-tuned model.
+  return { success: true };
 });
 
 exports.app = functions.https.onRequest(restfulApp);
